7166c5a4ec8752eff97bcc4c62503f02062b3874
BonnieSites.Solutions <info@bonnysites.com>
2023-09-02 16:17:29 -0400

    fixed proj name for auth

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import json
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
 
-db = firestore.Client(credentials=creds, project="streamlit-reddit")
+db = firestore.Client(credentials=creds, project="streamlit-reddit-5b36c")
 
 st.set_page_config(page_title="My Reddit App ")
 
