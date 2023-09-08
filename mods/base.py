316a86cb1c00abdb9cdb33f7b8f43393a5b977b1
BonnieSites.Solutions <info@bonnysites.com>
2023-09-08 08:00:36 -0400

    added new menu pages

--- mods/base.py
+++ mods/base.py
@@ -1,20 +1,25 @@
 import streamlit as st
 
 st.set_page_config(
-       page_title="My Reddit App",
-       layout="wide",
-       page_icon=":female-technologist:",
+       page_title='My Reddit App',
+       layout='wide',
+       page_icon=':female-technologist:',
+       initial_sidebar_state='expanded',
+       menu_items={
+              'Get Help': 'https://my-reddit.streamlit.app/',
+              'Report a bug': 'https://my-reddit.streamlit.app/',
+              'About': '# This is a header. '
+       }
 )
 
-custom_format = """
+custom_format = '''
        <style>
        body {
-       background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+       background-image: url('https://images.unsplash.com/photo-1542281286-9e0a16bb7366');
        background-size: cover;
        }
-       #MainMenu {visibility: hidden; }
        footer {visibility: hidden;}
        .css-1wbqy5l e17vllj40  {visibility: hidden; }
        </style>
-       """
+       '''
 st.markdown(custom_format, unsafe_allow_html=True)
---
+++ pages/02_My_Profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.markdown("# My Profile ")
+st.sidebar.markdown("# My Profile ")
+
+# Check for authenticated/logged in
+
+# If logged in/auth, go Home
+
+# If not logged in, show profile dashboard
+
+# TODO: stats on posting, replying, etc.
