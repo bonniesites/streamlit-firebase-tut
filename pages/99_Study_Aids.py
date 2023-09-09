f36fc045dba275dc6da1e5318da4ca84532cf2ab
BonnieSites.Solutions <info@bonnysites.com>
2023-09-08 21:56:41 -0400

    fixed part of menu

--- pages/99_Study_Aids.py
+++ pages/99_Study_Aids.py
@@ -1,5 +1,30 @@
 import streamlit as st
 from mods import base
+
+st.set_page_config(
+       page_title='BYU-I PATHWAY STUDY SKILLS',
+       layout='centered',
+       page_icon='💫',
+       initial_sidebar_state='collapsed',
+       menu_items={
+              'Get Help': 'https://my-reddit.streamlit.app/',
+              'Report a bug': 'https://my-reddit.streamlit.app/',
+              'About': '# This is a header. '
+       }
+)
+
+custom_format = '''
+       <style>
+       #root {
+       background-image: url('https://images.unsplash.com/photo-1688453756951-842a78eec6ad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2535&q=80');
+       background-size: cover;
+       }
+       footer {visibility: hidden;}
+       .css-1wbqy5l e17vllj40  {visibility: hidden; }
+       </style>
+       '''
+st.markdown(custom_format, unsafe_allow_html=True)
+
 st.title('BYU-I PATHWAY STUDY SKILLS')
 st.divider()
 
@@ -20,5 +45,5 @@ urls = {
 
 with st.container():
     for key, value in urls.items():
-        st.subheader(f'{key}: {value}', divider=False)
+        st.markdown(f'{key}: {value}')
 
